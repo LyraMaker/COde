87cbecb31e3258eb20df1635ac5cf8fcb5575d94
LyraMaker <gabriel.lyrat@gmail.com>
2023-03-14 09:17:42 -0300

    Atualização da ultima aula

--- condicao/condicao.js
+++ condicao/condicao.js
@@ -3,8 +3,7 @@ var contador = 0
 while(contador<100){
 var x = prompt("Digite um número");
 
-if(x>0){
-    alert("O "+x+" é um número positivo")
+ieert("O "+x+" é um número positivo")
 }else if(x<0){
     alert("O "+x+" é um número negativo")
 }else{
---
+++ query/query.js
@@ -0,0 +1,2 @@
+var escola = document.querySelector(".escola");
+escola.textContent = "3D Colégios"
