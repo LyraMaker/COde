c1b9ef5e0cb108d59ed58c77df9ceda17c95fcb4
LyraMaker <gabriel.lyrat@gmail.com>
2023-03-21 10:01:40 -0300

    Atividade do dia 21/03

---
+++ calculadora/calculadora.js
@@ -0,0 +1,13 @@
+function soma(){
+caixa1 = Number(document.querySelector("#txtN1").value)
+caixa2 = Number(document.querySelector("#txtN2").value)
+
+resultado = caixa1 + caixa2
+
+mostrarResultado(resultado)
+}
+
+function mostrarResultado(texto){
+    resultado = document.querySelector("#resultado")
+    resultado.textContent = texto
+}
