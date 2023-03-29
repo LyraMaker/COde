09b4c3b0b330fe11af2c2d51afc4d1be908ecae7
LyraMaker <gabriel.lyrat@gmail.com>
2023-03-29 08:15:00 -0300

    Atividade

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -1,40 +1,42 @@
-function soma(){
-caixa1 = Number(document.querySelector("#txtN1").value)
-caixa2 = Number(document.querySelector("#txtN2").value)
+var historico = []
 
-resultado = caixa1 + caixa2
-
-mostrarResultado(resultado)
-}
-
-function subtracao(){
+function soma() {
     caixa1 = Number(document.querySelector("#txtN1").value)
     caixa2 = Number(document.querySelector("#txtN2").value)
-    
-    resultado = caixa1 - caixa2
-    
-    mostrarResultado(resultado)
-    }
 
-function multiplicacao(){
-caixa1 = Number(document.querySelector("#txtN1").value)
-caixa2 = Number(document.querySelector("#txtN2").value)
-
-resultado = caixa1 * caixa2
-
-mostrarResultado(resultado)
-}
-
-function divisao(){
-    caixa1 = Number(document.querySelector("#txtN1").value)
-    caixa2 = Number(document.querySelector("#txtN2").value)
-    
     resultado = caixa1 + caixa2
-    
+    historico.push(resultado)
     mostrarResultado(resultado)
-    }
+}
 
-function mostrarResultado(texto){
+function subtracao() {
+    caixa1 = Number(document.querySelector("#txtN1").value)
+    caixa2 = Number(document.querySelector("#txtN2").value)
+
+    resultado = caixa1 - caixa2
+    historico.push(resultado)
+    mostrarResultado(resultado)
+}
+
+function multiplicacao() {
+    caixa1 = Number(document.querySelector("#txtN1").value)
+    caixa2 = Number(document.querySelector("#txtN2").value)
+
+    resultado = caixa1 * caixa2
+    historico.push(resultado)
+    mostrarResultado(resultado)
+}
+
+function divisao() {
+    caixa1 = Number(document.querySelector("#txtN1").value)
+    caixa2 = Number(document.querySelector("#txtN2").value)
+
+    resultado = caixa1 + caixa2
+    historico.push(resultado)
+    mostrarResultado(resultado)
+}
+
+function mostrarResultado(texto) {
     resultado = document.querySelector("#resultado")
-    resultado.textContent = texto
+    resultado.innerHTML += "<p>"+texto+"</p>"
 }
---
+++ escutando eventos/escutador.js
@@ -0,0 +1,5 @@
+var titulo = document.querySelector('h1')
+
+titulo.addEventListener('click',function(){
+    alert("Eu sou um título!")
+})
